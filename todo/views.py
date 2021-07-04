300a62e6ba2aa9334cb5c4234967010c56806671
gwenjo <jo.gwen@hotmail.com>
2021-07-04 09:33:55 +0000

    learn Django

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Item
-
-# Create your views here.
+from .forms import ItemForm
 
 
 def get_todo_list(request):
@@ -13,4 +12,14 @@ def get_todo_list(request):
 
 
 def add_item(request):
+    if request.method == 'POST':
+        name = request.POST.get('item_name')
+        done = 'done' in request.POST
+        Item.objects.create(name=name, done=done)
+
+        return redirect('get_todo_list')
+    form = ItemForm()
+    context = {
+        'form': form
+    }
     return render(request, 'todo/add_item.html')
